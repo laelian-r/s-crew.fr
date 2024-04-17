4f690f32af86b9cd056b199169de5686c758b00f
laelian-r <laelian.roux@gmail.com>
2024-04-17 16:28:10 +0200

    Reset CSS - Change ul li to Article

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
 import articles from './articles.json';
 const section = document.querySelector('#articles');
 
-let ul = document.createElement('ul');
-section.appendChild(ul);
+let articleElement = document.createElement('article');
+section.appendChild(articleElement);
 
 fetch('./articles.json')
   .then(response => response.json())
   .then(data => {
     data.forEach(article => {
-      let li = document.createElement('li');
-      ul.appendChild(li);
-
       let img = document.createElement('img');
       img.setAttribute('src', article.image);
       let h3 = document.createElement('h3');
       let p = document.createElement('p');
-      li.appendChild(img);
-      li.appendChild(h3);
-      li.appendChild(p);
+      articleElement.appendChild(img);
+      articleElement.appendChild(h3);
+      articleElement.appendChild(p);
 
       h3.textContent = article.name;
       p.textContent = article.price;
-    });
+    })
   });
